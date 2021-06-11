17129ae2a8a527ea494d087fbad8a4c90bcd08b2
Karine Jappe <karine_jappe@hotmail.com>
2021-06-11 00:13:06 -0300

    Add. imagem a listagem de estabelecimentos.

--- src/pages/cliente/ListagemEstabelecimentos.js
+++ src/pages/cliente/ListagemEstabelecimentos.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import {
     StyleSheet,
+    Image,
     Text,
     View,
     StatusBar,
     SafeAreaView,
     FlatList
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { getEstabelecimento } from '../../services/estabelecimento';
 
 export default function Categorias() {
@@ -21,6 +23,17 @@ export default function Categorias() {
 
     const Item = ({ data }) => (
         <View style={styles.item}>
+            <View style={[styles.anexo, styles.button]}>
+                {data.imagem ?
+                    <Image
+                        source={{ uri: `data:image/png;base64,${data.imagem}` }}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                    : <Icon
+                        style={styles.icon} name="camera" size={80} color='#555'
+                    />
+                }
+            </View>
             <Text style={styles.title}>{data.nome_fantasia}</Text>
             <Text>{data.endereco}</Text>
         </View>
@@ -48,11 +61,28 @@ const styles = StyleSheet.create({
     },
     item: {
         backgroundColor: '#ddd',
-        padding: 20,
+        padding: 10,
+        borderRadius: 10,
         marginVertical: 8,
         marginHorizontal: 16,
     },
     title: {
         fontSize: 20,
+    },
+    button: {
+        borderRadius: 10,
+        height: 100,
+        backgroundColor: '#ffff',
+        color: 'black',
+        fontFamily: 'Poppins-Regular',
+        fontSize: 15
+    },
+    anexo: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    icon: {
+        alignItems: 'center'
     },
 });
