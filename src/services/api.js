0f7184b0f10b288eb0d7a53ea614c086b2f0d270
Karine Jappe <karine_jappe@hotmail.com>
2021-06-29 01:22:15 -0300

    Alteração URL banco produção.

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import { Alert } from 'react-native'
 import { getUser, navigate, deleteUser } from '../utils/asyncStorage';
 
 const api = axios.create({
-    baseURL: 'http://10.0.2.2:8000',
+    baseURL: 'https://api-borala-compras.herokuapp.com',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
